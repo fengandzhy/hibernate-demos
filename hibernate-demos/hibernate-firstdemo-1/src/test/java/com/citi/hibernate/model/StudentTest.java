2bb34459a08bac3e4cad6783d2ad77ffd2d60f00
fengandzhy <fengandzhy@gmail.com>
2019-08-08 22:29:44 +1200

    first demo for hibernate

--- hibernate-demos/hibernate-firstdemo-1/src/test/java/com/citi/hibernate/model/StudentTest.java
+++ hibernate-demos/hibernate-firstdemo-1/src/test/java/com/citi/hibernate/model/StudentTest.java
@@ -2,7 +2,11 @@ package com.citi.hibernate.model;
 
 import static org.junit.Assert.*;
 
+import java.util.Date;
+
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.service.ServiceRegistry;
@@ -19,25 +23,18 @@ public class StudentTest {
 		ServiceRegistry serviceRegistry = configuration.getStandardServiceRegistryBuilder().build();		
 		sessionFactory = new MetadataSources(serviceRegistry).buildMetadata().buildSessionFactory();
 		/*②通过工厂类开启Session对象*/
-
+		Session session = sessionFactory.openSession();		
 		/*③开始事务*/
-
+		Transaction transaction = session.beginTransaction();
 		/*④执行数据库的操作*/
-
+		Student stu = new Student("sam","male",new Date());
+		session.save(stu);
+		
 		/*⑤提交事务*/
-
+		transaction.commit();
 		/*⑥关闭Session*/
-
+		session.close();
 		/*⑦关闭工厂类*/
-
-		
-		
-		
-		/**包含配置文件里的信息*/
-		
-		
-		
-		
+		sessionFactory.close();		
 	}
-
 }
