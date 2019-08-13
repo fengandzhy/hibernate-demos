3434423090076176a9cdf6216b1f9fe48a1dbf86
fengandzhy <fengandzhy@gmail.com>
2019-08-14 08:01:58 +1200

    test case for hibernate session

---
+++ hibernate-demos/hibernate-session-1/src/test/java/com/citi/hibernate/model/UserTest.java
@@ -0,0 +1,29 @@
+package com.citi.hibernate.model;
+
+import static org.junit.Assert.*;
+
+import org.hibernate.SessionFactory;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class UserTest {
+
+	SessionFactory sessionFactory = null;
+	
+	@Before
+	public void init() {
+		System.out.println("init");
+	}
+	
+	@After
+	public void destory() {
+		
+	}
+	
+	@Test
+	public void test() {
+		
+	}
+
+}
