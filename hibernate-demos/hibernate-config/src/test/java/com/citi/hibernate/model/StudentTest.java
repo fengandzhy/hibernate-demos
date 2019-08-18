09b52a59aed676590e8f58bdc57a00ac86a96f54
fengandzhy <fengandzhy@gmail.com>
2019-08-19 07:38:51 +1200

    hibernate config

---
+++ hibernate-demos/hibernate-config/src/test/java/com/citi/hibernate/model/StudentTest.java
@@ -0,0 +1,55 @@
+package com.citi.hibernate.model;
+
+import static org.junit.Assert.*;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Date;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.jdbc.Work;
+import org.hibernate.service.ServiceRegistry;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class StudentTest {
+
+	SessionFactory sessionFactory = null;
+	Session session = null;
+	Transaction transaction = null;
+	
+	@Before
+	public void init() {
+		Configuration configuration = new Configuration().configure();
+		ServiceRegistry serviceRegistry = configuration.getStandardServiceRegistryBuilder().build();
+		sessionFactory = new MetadataSources(serviceRegistry).buildMetadata().buildSessionFactory();
+		session = sessionFactory.openSession();
+		transaction = session.beginTransaction();		
+	}	
+	
+	@Test
+	public void doWorkTest() {
+		session.doWork(new Work() {
+
+			public void execute(Connection connection) throws SQLException {
+				System.out.println(connection);
+				
+			}
+			
+		});
+	}
+	
+	
+	@After
+	public void destory() {
+		transaction.commit();//commit只会针对持久对象，判断其是否跟数据库一致,不一致发起sql语句操作数据库，本质上还是flash()方法在起作用
+		session.close();
+		sessionFactory.close();
+	}
+
+}
