39cc3a64c2f8bcdbfe1c0d96949d071cfb948519
fengandzhy <fengandzhy@gmail.com>
2019-08-30 07:39:43 +1200

    hibernate many to many demo

---
+++ hibernate-demos/hibernate-manytomany-1/src/test/java/ManyToManyTest.java
@@ -0,0 +1,32 @@
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+import org.junit.After;
+import org.junit.Before;
+
+public class ManyToManyTest {
+	SessionFactory sessionFactory = null;
+	Session session = null;
+	Transaction transaction = null;
+	
+	@Before
+	public void init() {
+		Configuration configuration = new Configuration().configure();
+		ServiceRegistry serviceRegistry = configuration.getStandardServiceRegistryBuilder().build();
+		sessionFactory = new MetadataSources(serviceRegistry).buildMetadata().buildSessionFactory();
+		session = sessionFactory.openSession();
+		transaction = session.beginTransaction();		
+	}
+	
+	
+	
+	@After
+	public void destory() {
+		transaction.commit();//commit只会针对持久对象，判断其是否跟数据库一致,不一致发起sql语句操作数据库，本质上还是flash()方法在起作用
+		session.close();
+		sessionFactory.close();
+	}
+}
