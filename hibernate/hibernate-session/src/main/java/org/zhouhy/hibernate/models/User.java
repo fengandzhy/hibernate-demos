328dd42ee2a22373ce141b14ab2383a1c7d12afa
fengandzhy <fengandzhy@gmail.com>
2022-03-22 22:48:20 +1300

    hibernate-session

---
+++ hibernate/hibernate-session/src/main/java/org/zhouhy/hibernate/models/User.java
@@ -0,0 +1,40 @@
+package org.zhouhy.hibernate.models;
+
+public class User {
+	private String id;
+	private String username;
+	private String password;
+	
+	public User() {
+		super();
+	}	
+	public User(String username, String password) {
+		super();		
+		this.username = username;
+		this.password = password;
+	}
+
+
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", username=" + username + ", password=" + password + "]";
+	}	
+}
---
+++ hibernate/hibernate-session/src/test/java/org/zhouhy/hibernate/models/UserTest.java
@@ -0,0 +1,52 @@
+package org.zhouhy.hibernate.models;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.internal.SessionImpl;
+import org.hibernate.service.ServiceRegistry;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.sql.SQLException;
+
+public class UserTest {
+
+    private static final Logger logger = LoggerFactory.getLogger(UserTest.class);
+    
+    SessionFactory sessionFactory = null;
+    Session session = null;
+    Transaction transaction = null;
+
+    @Before
+    public void init() {
+        Configuration configuration = new Configuration().configure();
+        ServiceRegistry serviceRegistry = configuration.getStandardServiceRegistryBuilder().build();
+        sessionFactory = new MetadataSources(serviceRegistry).buildMetadata().buildSessionFactory();
+        session = sessionFactory.openSession();
+        transaction = session.beginTransaction();        
+    }
+
+    @Test
+    public void testSave() throws SQLException {
+        User user = new User("sam","111111");
+        System.out.println(user);
+        session.save(user);
+        session.flush();
+        SessionImpl sessionImpl = (SessionImpl) session;
+        System.out.println(sessionImpl.connection().getAutoCommit());
+        transaction.rollback();
+    }
+
+//    @After
+    public void destory() {
+        transaction.commit();
+        session.close();
+        sessionFactory.close();
+    }
+}
