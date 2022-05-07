948f641464e1824c7e4904332248647a9d19077f
fengandzhy <fengandzhy@gmail.com>
2022-05-07 16:58:09 +1200

    hibernate session

--- hibernate/hibernate-session/src/test/java/org/zhouhy/hibernate/models/StudentTest.java
+++ hibernate/hibernate-session/src/test/java/org/zhouhy/hibernate/models/StudentTest.java
@@ -12,11 +12,32 @@ public class StudentTest extends AbstractTest{
         try {
             logger.info(stu.toString());
             session.save(stu);
+            stu.setName("Frank");
             logger.info(stu.toString());
             transaction.commit();
         } catch (Exception e) {
             e.printStackTrace();
             transaction.rollback();
         }
-    }   
+    }
+
+    /**
+     *  这里的session.save(stu);并不发出insert语句, insert语句是在transaction.commit();之时发出的, 此时已经执行过session.clear();
+     *  session缓存已经被清空, 所以这里不发出insert 语句, 这里也就不会向数据库插入记录了.
+     * */
+    @Test
+    public void testSaveWithClear(){
+        transaction = session.beginTransaction();
+        Student stu = new Student("sam","male",new Date());
+        try {
+            logger.info(stu.toString());
+            session.save(stu);
+            session.clear();
+            logger.info(stu.toString());
+            transaction.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            transaction.rollback();
+        }
+    }
 }
--- hibernate/hibernate-session/src/test/java/org/zhouhy/hibernate/models/UserTest.java
+++ hibernate/hibernate-session/src/test/java/org/zhouhy/hibernate/models/UserTest.java
@@ -1,39 +1,18 @@
 package org.zhouhy.hibernate.models;
 
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
-import org.hibernate.boot.MetadataSources;
-import org.hibernate.cfg.Configuration;
-import org.hibernate.service.ServiceRegistry;
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-
-
-public class UserTest {
-
-    private static final Logger logger = LoggerFactory.getLogger(UserTest.class);
+public class UserTest extends AbstractTest{
     
-    SessionFactory sessionFactory = null;
-    Session session = null;
-    Transaction transaction = null;
-
-    @Before
-    public void init() {
-        Configuration configuration = new Configuration().configure();
-        ServiceRegistry serviceRegistry = configuration.getStandardServiceRegistryBuilder().build();
-        sessionFactory = new MetadataSources(serviceRegistry).buildMetadata().buildSessionFactory();
-        session = sessionFactory.openSession();             
-    }
-
     /**
      * 1. 当一个对象执行了save方法之后又这样几个变化
      *  1) 它被纳入到了session 的缓存, 受session 管理
      *  2) hibernate 会给它一个id 这个id就是它插入到数据库之后的id, 它如果原先没有id 它会给它生成一个id 但如果有id 了 它还会重新给它分配一个id 
+     * 2. 另外save方法是否发起insert 语句取决于这个 主键生成方式 比方说如果主键生成的方式是native, 那么它会在调用save 方法直接发出insert
+     * 如果主键生成的方式是uuid, 执行save方法的时候不会发起insert 只有在flush 方法清理session缓存的时候才会发起insert
+     * 3. 在调用了save之后, 也就是对象已经是持久状态了, 就不能在setId 了，如果 setId 会报错
+     * 4. 在save方法之后，再去修改其他，例如用户名，user.setUsername("Frank"); 虽然user对象此刻未表现在数据库, 
+     * 但是你修改了持久状态的某个属性(这个属性绝对不能是ID), 它仍然要去执行一个update语句 
      * 
      * */
     @Test
@@ -44,13 +23,37 @@ public class UserTest {
         try {
             logger.info(user.toString());
             session.save(user);
+//            user.setUsername("Frank");
             logger.info(user.toString());
             transaction.commit();            
         } catch (Exception e) {
             e.printStackTrace();
             transaction.rollback();
         }     
-    }    
+    }
+    
+    /**
+     * 1. 注意这里的clear方法记录插入了但是没有执行修改, 这是因为当你执行session.save(user); 它已经发出了insert 语句
+     * 这是因为这里的 id 生成策略是 native. 而 session.clear(); 发生在发出insert 语句之后, 所以当commit的时候就体现在了数据库里. 
+     * 但是这的 user.setUsername("Frank"); 所引发的update 语句却是在commit之时发出的, 它发生在session.clear();之后 所以
+     * 它不被执行. 因为此时 session 已经被清空.  
+     * */
+    @Test
+    public void testSaveWithClear(){
+        transaction = session.beginTransaction();
+        User user = new User("sam","111111");
+        try {
+            logger.info(user.toString());
+            session.save(user);
+            user.setUsername("Frank");
+            session.clear();
+            logger.info(user.toString());
+            transaction.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            transaction.rollback();
+        }
+    }
     
     /**
      * get 方法 是根据 记录的ID 取数据 以下代码虽然执行两次get 方法, 但是对于数据库只产生了一条 select SQL语句
@@ -68,7 +71,7 @@ public class UserTest {
      *  1 这里的flush 方法是清理缓存的操作,执行一系列的SQL语句,但不会提交事务; 它只是发起sql语句,但是并不会真正的影响数据库里的
      *  记录除了 执行到 transaction.commit(); 或者 session.close(); 数据的变化才能在数据库中体现出来.
      *  例如本例，你把user的 password给改了，那么就意味着 缓存里的user和数据库里的记录不一样了, 所以 执行到 session.flush();
-     *  的时候会有一条sql语句体现出来, 但是数据库里的记录并没有改变, 直到commit之后     *   
+     *  的时候会有一条 update sql语句体现出来, 但是数据库里的记录并没有改变, 直到commit之后     *   
      * */
     @Test
     public void testFlush(){
@@ -111,10 +114,4 @@ public class UserTest {
         logger.info(user.toString());
         transaction.commit();
     }    
-
-    @After
-    public void destroy() {        
-        session.close();
-        sessionFactory.close();
-    }
 }
