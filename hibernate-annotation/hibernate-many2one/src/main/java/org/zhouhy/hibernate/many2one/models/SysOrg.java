7e21ec31dd29b2e408607ad530e5d1b8cd43bc03
fengandzhy <fengandzhy@gmail.com>
2022-07-07 09:04:57 +1200

    hibernate-many2one

--- hibernate-annotation/hibernate-many2one/src/main/java/org/zhouhy/hibernate/many2one/models/SysOrg.java
+++ hibernate-annotation/hibernate-many2one/src/main/java/org/zhouhy/hibernate/many2one/models/SysOrg.java
@@ -9,4 +9,23 @@ public class SysOrg {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
+    @Column(name = "ORG_NAME", length = 60)
+    private String orgName;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getOrgName() {
+        return orgName;
+    }
+
+    public void setOrgName(String orgName) {
+        this.orgName = orgName;
+    }
 }
--- hibernate-annotation/hibernate-many2one/src/main/java/org/zhouhy/hibernate/many2one/models/SysUser.java
+++ hibernate-annotation/hibernate-many2one/src/main/java/org/zhouhy/hibernate/many2one/models/SysUser.java
@@ -8,6 +8,57 @@ public class SysUser {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
+    @Column(name = "USER_CODE", nullable = false, length = 40)
+    private String userCode;		
     
-    
+    @Column(name = "USER_NAME", length = 60)
+    private String userName;			
+
+    @Column(name = "DEPT_ID", length = 40)
+    private String deptId;				
+
+    @ManyToOne
+    @JoinColumn(name = "DEPT_ID", insertable = false, updatable = false, nullable=true)
+    private SysOrg dept;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUserCode() {
+        return userCode;
+    }
+
+    public void setUserCode(String userCode) {
+        this.userCode = userCode;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getDeptId() {
+        return deptId;
+    }
+
+    public void setDeptId(String deptId) {
+        this.deptId = deptId;
+    }
+
+    public SysOrg getDept() {
+        return dept;
+    }
+
+    public void setDept(SysOrg dept) {
+        this.dept = dept;
+    }
 }
