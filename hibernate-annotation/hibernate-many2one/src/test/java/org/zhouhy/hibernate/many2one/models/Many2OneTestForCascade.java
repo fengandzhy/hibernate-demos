135b02466aa349027793b8ed53bd6ce758e36898
fengandzhy <fengandzhy@gmail.com>
2022-12-30 21:32:02 +1300

    hibernate-many2one update the tests

---
+++ hibernate-annotation/hibernate-many2one/src/test/java/org/zhouhy/hibernate/many2one/models/Many2OneTestForCascade.java
@@ -0,0 +1,7 @@
+package org.zhouhy.hibernate.many2one.models;
+
+
+public class Many2OneTestForCascade extends Many2OneTest{
+
+    
+}
---
+++ hibernate-annotation/hibernate-many2one/src/test/java/org/zhouhy/hibernate/many2one/models/Many2OneTestForFetch.java
@@ -0,0 +1,4 @@
+package org.zhouhy.hibernate.many2one.models;
+
+public class Many2OneTestForFetch extends Many2OneTest{
+}
