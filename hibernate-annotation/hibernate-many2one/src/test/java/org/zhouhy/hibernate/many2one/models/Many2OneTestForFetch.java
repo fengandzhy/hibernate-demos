7a9836349321592f877774a194bcafb3435a460e
fengandzhy <fengandzhy@gmail.com>
2022-12-31 20:06:45 +1300

    hibernate-many2one update the tests

--- hibernate-annotation/hibernate-many2one/src/test/java/org/zhouhy/hibernate/many2one/models/Many2OneTestForFetch.java
+++ hibernate-annotation/hibernate-many2one/src/test/java/org/zhouhy/hibernate/many2one/models/Many2OneTestForFetch.java
@@ -1,6 +1,15 @@
 package org.zhouhy.hibernate.many2one.models;
 
+import org.junit.Test;
+
 public class Many2OneTestForFetch extends Many2OneTest{
-    
-    
+
+    /**
+     * @ManyToOne 默认的FetchMode.JOIN 然后 FetchType.EAGER
+     * */
+    @Test
+    public void testGet1(){
+        Article a1 = session.get(Article.class,25L);
+        logger.info(a1.getAuthor().getId()+"");
+    }
 }
