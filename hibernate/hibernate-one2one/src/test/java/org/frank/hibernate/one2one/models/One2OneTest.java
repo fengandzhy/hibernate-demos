4e33deeeb35be887bff41ef186b872a32a68d21f
fengandzhy <fengandzhy@gmail.com>
2023-01-08 11:14:23 +1300

    hibernate-one2one add One2OneTest.java

---
+++ hibernate/hibernate-one2one/src/test/java/org/frank/hibernate/one2one/models/One2OneTest.java
@@ -0,0 +1,35 @@
+package org.frank.hibernate.one2one.models;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+import org.junit.After;
+import org.junit.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+public abstract class One2OneTest {
+
+    protected final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    private SessionFactory sessionFactory = null;
+    protected Session session = null;
+
+    @Before
+    public void init() {
+        Configuration configuration = new Configuration().configure();
+        ServiceRegistry serviceRegistry = configuration.getStandardServiceRegistryBuilder().build();
+        sessionFactory = new MetadataSources(serviceRegistry).buildMetadata().buildSessionFactory();
+        session = sessionFactory.openSession();
+    }
+
+
+    @After
+    public void destroy() {
+        session.close();
+        sessionFactory.close();
+    }
+}
