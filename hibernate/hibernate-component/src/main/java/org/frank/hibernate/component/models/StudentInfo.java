a661e867935c54b0e9e45d631cfb2ff2d5f7223a
fengandzhy <fengandzhy@gmail.com>
2023-01-09 06:06:19 +1300

    hibernate-component hibernate-component code.

---
+++ hibernate/hibernate-component/src/main/java/org/frank/hibernate/component/models/StudentInfo.java
@@ -0,0 +1,34 @@
+package org.frank.hibernate.component.models;
+
+import java.util.Date;
+
+public class StudentInfo {
+    
+    private String address;
+    private String email;
+    private Date birthday;
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+}
