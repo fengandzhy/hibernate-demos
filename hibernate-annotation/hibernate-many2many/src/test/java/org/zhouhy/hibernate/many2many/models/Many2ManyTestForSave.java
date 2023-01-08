d7c5f052ab98710e46efeb1b140f21f1234143e8
fengandzhy <fengandzhy@gmail.com>
2023-01-08 20:35:36 +1300

    hibernate-one2one update Role.java and Many2ManyTestForSave.java

--- hibernate-annotation/hibernate-many2many/src/test/java/org/zhouhy/hibernate/many2many/models/Many2ManyTestForSave.java
+++ hibernate-annotation/hibernate-many2many/src/test/java/org/zhouhy/hibernate/many2many/models/Many2ManyTestForSave.java
@@ -21,13 +21,40 @@ public class Many2ManyTestForSave extends Many2ManyTest{
         u1.setUsername("b1");
         u1.setPassword("1");
         
-        r1.getUsers().add(u1);
-//        u1.getRoles().add(r1); // 由于两边都是 @JoinTable 所以只能设置单边的关系.
+//        r1.getUsers().add(u1);
+        u1.getRoles().add(r1); // 由于两边都是 @JoinTable 所以只能设置单边的关系.
         
         session.save(u1);
         session.save(r1);
         
         
+        if (transaction.getStatus().equals(TransactionStatus.ACTIVE)){
+            transaction.commit();
+        }
+    }
+
+    /**
+     * 如果在Role中设置了mappedBy 表示Role 不再维护 t1_user_role,  所以设置role的关联关系r1.getUsers().add(u1); 不会再对t1_user_role 有作用
+     * 
+     *  一般情况下，我个人倾向于两边同时设置 @JoinTable, 保存的时候只设置一端的 关联关系.
+     * */
+    @Test
+    public void testSaveForRoleInverse(){
+        Transaction transaction = session.beginTransaction();
+        Role r1 = new Role();
+        r1.setName("a3");
+
+        User u1 = new User();
+        u1.setUsername("b3");
+        u1.setPassword("1");
+
+//        r1.getUsers().add(u1);
+        u1.getRoles().add(r1); // 由于两边都是 @JoinTable 所以只能设置单边的关系.
+
+        session.save(u1);
+        session.save(r1);
+
+
         if (transaction.getStatus().equals(TransactionStatus.ACTIVE)){
             transaction.commit();
         }
