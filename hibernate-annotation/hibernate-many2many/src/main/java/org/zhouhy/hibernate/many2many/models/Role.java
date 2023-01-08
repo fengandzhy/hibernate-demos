817e406a452904fdc05ef7c013cb2b32662ca8a7
fengandzhy <fengandzhy@gmail.com>
2023-01-08 19:06:01 +1300

    hibernate-one2one standard configuration

--- hibernate-annotation/hibernate-many2many/src/main/java/org/zhouhy/hibernate/many2many/models/Role.java
+++ hibernate-annotation/hibernate-many2many/src/main/java/org/zhouhy/hibernate/many2many/models/Role.java
@@ -1,19 +1,47 @@
 package org.zhouhy.hibernate.many2many.models;
 
 import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity(name = "Role")
-@Table(name = "t1_role1")
+@Table(name = "t1_role")
 public class Role {
-    @Id
+    @Id@Column(name="role_id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Long roleId;
 
-    public Long getId() {
-        return id;
+    @Column(name = "username", unique=true)
+    private String name;
+
+    
+    @ManyToMany 
+    @JoinTable(name="t1_user_role",
+            joinColumns = @JoinColumn(referencedColumnName="role_id",name="role_id"),
+            inverseJoinColumns = @JoinColumn(referencedColumnName="user_id",name="user_id"))
+    private Set<User> users = new HashSet<>();
+
+    public String getName() {
+        return name;
     }
 
-    public void setId(Long id) {
-        this.id = id;
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(Set<User> users) {
+        this.users = users;
+    }
+
+    public Long getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(Long roleId) {
+        this.roleId = roleId;
     }
 }
--- hibernate-annotation/hibernate-many2many/src/main/java/org/zhouhy/hibernate/many2many/models/User.java
+++ hibernate-annotation/hibernate-many2many/src/main/java/org/zhouhy/hibernate/many2many/models/User.java
@@ -1,28 +1,59 @@
 package org.zhouhy.hibernate.many2many.models;
 
 import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity(name = "User")
-@Table(name = "t1_user1")
+@Table(name = "t1_user")
 public class User {
     
-    @Id
+    @Id@Column(name="user_id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Long userId;
 
     @Column(name = "username", unique=true)
     private String username;
 
     @Column(name = "password")
     private String password;
-    
-    
 
-    public Long getId() {
-        return id;
+    
+    @ManyToMany    
+    @JoinTable(name="t1_user_role",            
+            joinColumns = @JoinColumn(referencedColumnName="user_id",name="user_id"),
+            inverseJoinColumns = @JoinColumn(referencedColumnName="role_id",name="role_id"))
+    private Set<Role> roles = new HashSet<>();
+
+    public String getUsername() {
+        return username;
     }
 
-    public void setId(Long id) {
-        this.id = id;
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
     }
 }
