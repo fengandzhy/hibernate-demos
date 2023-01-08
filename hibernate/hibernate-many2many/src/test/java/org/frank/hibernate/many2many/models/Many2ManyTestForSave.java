128f75299d68af77647c902389b6db9d85e8743a
fengandzhy <fengandzhy@gmail.com>
2023-01-08 16:58:42 +1300

    hibernate-many2many update Many2ManyTestForSave.java and Course.hbm.xml

--- hibernate/hibernate-many2many/src/test/java/org/frank/hibernate/many2many/models/Many2ManyTestForSave.java
+++ hibernate/hibernate-many2many/src/test/java/org/frank/hibernate/many2many/models/Many2ManyTestForSave.java
@@ -4,6 +4,9 @@ import org.hibernate.Transaction;
 import org.hibernate.resource.transaction.spi.TransactionStatus;
 import org.junit.Test;
 
+import javax.persistence.PersistenceException;
+
+
 public class Many2ManyTestForSave extends Many2ManyTest{
     
     
@@ -49,6 +52,40 @@ public class Many2ManyTestForSave extends Many2ManyTest{
         c2.getTeachers().add(t2);
         c1.getTeachers().add(t2);
         c2.getTeachers().add(t1);
+
+        session.save(c1);
+        session.save(c2);
+        session.save(t1);
+        session.save(t2);
+
+        if (transaction.getStatus().equals(TransactionStatus.ACTIVE)){
+            transaction.commit();
+        }
+    }
+
+    /**
+     * 当双方都不设置inverse=true的时候, 这个时候两方都要维护t_teacher_course表, 如果这时彼此双方都设置了关联关系的话，就会抛出异常,
+     * 所以，只能去掉一方的关联.
+     * 
+     * 
+     * */
+    @Test(expected = PersistenceException.class)
+    public void testSaveForNoInverse(){
+        Transaction transaction = session.beginTransaction();
+        Teacher t1 = new Teacher("a1","t1");
+        Teacher t2 = new Teacher("a2", "t2");
+        Course c1 = new Course("b1","c1");
+        Course c2 = new Course("b2","c2");
+        
+        c1.getTeachers().add(t1);
+        c2.getTeachers().add(t2);
+        c1.getTeachers().add(t2);
+        c2.getTeachers().add(t1);
+
+        t1.getCourses().add(c1);
+        t2.getCourses().add(c2);
+        t1.getCourses().add(c2);
+        t2.getCourses().add(c1);
 
         session.save(c1);
         session.save(c2);
