0ad5284c65e78a932f371c95f410d5f83fba85e8
fengandzhy <fengandzhy@gmail.com>
2023-01-15 16:52:32 +1300

    hibernate-component update ComponentTestForSave.java

--- hibernate-annotation/hibernate-component/src/test/java/org/frank/hibernate/component/models/ComponentTestForSave.java
+++ hibernate-annotation/hibernate-component/src/test/java/org/frank/hibernate/component/models/ComponentTestForSave.java
@@ -1,11 +1,26 @@
 package org.frank.hibernate.component.models;
 
+import org.hibernate.Transaction;
+import org.hibernate.resource.transaction.spi.TransactionStatus;
 import org.junit.Test;
+
+import java.util.GregorianCalendar;
 
 public class ComponentTestForSave extends ComponentTest{
     
     @Test
     public void testSave(){
-        logger.info("The test is running.");
+        Transaction transaction = session.beginTransaction();
+        StudentInfo info = new StudentInfo();
+        info.setAddress("Address");
+        info.setBirthday( new GregorianCalendar(2014, 2, 11).getTime());
+        info.setEmail("aabb@ccdd.com");
+        Student s = new Student();
+        s.setName("Wang");
+        s.setInfo(info);
+        session.save(s);
+        if (transaction.getStatus().equals(TransactionStatus.ACTIVE)){
+            transaction.commit();
+        }
     }
 }
