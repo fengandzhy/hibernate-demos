a0bd6dee4d516dff85eec2ad6addab2831dafeb9
fengandzhy <fengandzhy@gmail.com>
2023-01-15 18:36:57 +1300

    hibernate-inheritance update WindowFile.java and InheritanceSingleSaveTest.java

--- hibernate-annotation/hibernate-inheritance/src/test/java/org/frank/hibernate/inheritance/models/single/InheritanceSingleSaveTest.java
+++ hibernate-annotation/hibernate-inheritance/src/test/java/org/frank/hibernate/inheritance/models/single/InheritanceSingleSaveTest.java
@@ -1,6 +1,8 @@
 package org.frank.hibernate.inheritance.models.single;
 
 import org.frank.hibernate.inheritance.models.InheritanceTest;
+import org.hibernate.Transaction;
+import org.hibernate.resource.transaction.spi.TransactionStatus;
 import org.junit.Test;
 
 import java.util.GregorianCalendar;
@@ -8,10 +10,43 @@ import java.util.GregorianCalendar;
 public class InheritanceSingleSaveTest extends InheritanceTest {
     
     @Test
-    public void testWindowFile(){
+    public void testSaveWindowFile(){
+        Transaction transaction = session.beginTransaction();
         WindowFile windowFile = new WindowFile();
-        windowFile.setDate(new GregorianCalendar(2012,12,3).getTime());
+        windowFile.setDate(new GregorianCalendar(2012,0,3).getTime());
         windowFile.setName("windowFile1");
-        
+        windowFile.setType("A");
+        session.save(windowFile);
+        if (transaction.getStatus().equals(TransactionStatus.ACTIVE)){
+            transaction.commit();
+        }        
+    }
+
+    @Test
+    public void testSaveDocument(){
+        Transaction transaction = session.beginTransaction();
+        Document document = new Document();
+        document.setDate(new GregorianCalendar(2012,2,3).getTime());
+        document.setName("Document1");
+        document.setType("B");
+        document.setSize("10KB");
+        session.save(document);
+        if (transaction.getStatus().equals(TransactionStatus.ACTIVE)){
+            transaction.commit();
+        }
+    }
+
+    @Test
+    public void testSaveFolder(){
+        Transaction transaction = session.beginTransaction();
+        Folder folder = new Folder();
+        folder.setDate(new GregorianCalendar(2012,4,3).getTime());
+        folder.setName("Folder1");
+        folder.setType("B");
+        folder.setFileCount(3);
+        session.save(folder);
+        if (transaction.getStatus().equals(TransactionStatus.ACTIVE)){
+            transaction.commit();
+        }
     }
 }
