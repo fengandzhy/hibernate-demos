e362674f038043f46ae858051b3436173db27e4b
fengandzhy <fengandzhy@gmail.com>
2023-01-18 17:15:11 +1300

    hibernate-collections update CollectionTestForSetGet.java

--- hibernate/hibernate-collections/src/test/java/org/frank/hibernate/collections/models/CollectionTestForSetGet.java
+++ hibernate/hibernate-collections/src/test/java/org/frank/hibernate/collections/models/CollectionTestForSetGet.java
@@ -1,5 +1,7 @@
 package org.frank.hibernate.collections.models;
 
+import org.hibernate.Transaction;
+import org.hibernate.resource.transaction.spi.TransactionStatus;
 import org.junit.Test;
 
 public class CollectionTestForSetGet extends CollectionsTest{
@@ -8,5 +10,18 @@ public class CollectionTestForSetGet extends CollectionsTest{
     public void testGetForSetOrderBy(){
         SysUser sysUser = session.get(SysUser.class,1L);
         logger.info(sysUser.toString());
+    }
+
+    /**
+     * 这种方式可以修改
+     * */
+    @Test
+    public void testGetForSetAndUpdate(){
+        Transaction transaction = session.beginTransaction();
+        SysUser sysUser = session.get(SysUser.class,1L);
+        sysUser.getAddressSet().add("广州");
+        if (transaction.getStatus().equals(TransactionStatus.ACTIVE)){
+            transaction.commit();
+        }
     }
 }
