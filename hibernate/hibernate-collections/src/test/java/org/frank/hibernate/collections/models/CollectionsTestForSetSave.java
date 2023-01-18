0c94f2d93603a064c4904620071ad763ae956b0c
fengandzhy <fengandzhy@gmail.com>
2023-01-18 16:31:54 +1300

    hibernate-collections code and xml files

---
+++ hibernate/hibernate-collections/src/test/java/org/frank/hibernate/collections/models/CollectionsTestForSetSave.java
@@ -0,0 +1,27 @@
+package org.frank.hibernate.collections.models;
+
+import org.hibernate.Transaction;
+import org.hibernate.resource.transaction.spi.TransactionStatus;
+import org.junit.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class CollectionsTestForSetSave extends CollectionsTest{
+    
+    @Test
+    public void testSaveSet(){
+        Transaction transaction = session.beginTransaction();
+        Set<String> address = new HashSet<>();
+        address.add("北京");
+        address.add("上海");
+        
+        SysUser sysUser = new SysUser();
+        sysUser.setAddressSet(address);
+        sysUser.setName("王刚");
+        session.save(sysUser);
+        if (transaction.getStatus().equals(TransactionStatus.ACTIVE)){
+            transaction.commit();
+        }
+    }
+}
