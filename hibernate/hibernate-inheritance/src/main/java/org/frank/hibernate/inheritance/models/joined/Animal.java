1c26e9b01550fea1bdbbdb6a4b5f163b5a1e6d36
fengandzhy <fengandzhy@gmail.com>
2023-01-18 13:47:48 +1300

    hibernate-inheritance joined model code

---
+++ hibernate/hibernate-inheritance/src/main/java/org/frank/hibernate/inheritance/models/joined/Animal.java
@@ -0,0 +1,35 @@
+package org.frank.hibernate.inheritance.models.joined;
+
+public class Animal {
+
+    
+    private Integer id;
+    
+    private String name;
+    
+    private String color;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+}
