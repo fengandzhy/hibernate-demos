00b57ab382c2cbde4fe262fdf2aa8a0bb5a5c78c
fengandzhy <fengandzhy@gmail.com>
2023-10-11 14:44:16 +1300

    hibernate-collections map code.

--- hibernate/hibernate-collections/src/test/java/org/frank/hibernate/collections/models/CollectionsTestForMapSave.java
+++ hibernate/hibernate-collections/src/test/java/org/frank/hibernate/collections/models/CollectionsTestForMapSave.java
@@ -4,12 +4,21 @@ import org.hibernate.Transaction;
 import org.hibernate.resource.transaction.spi.TransactionStatus;
 import org.junit.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class CollectionsTestForMapSave extends CollectionsTest{
     
     @Test
     public void testSave(){
         Transaction transaction = session.beginTransaction();
-        
+        Map<String, String> addressMap = new HashMap<>();
+        addressMap.put("0001","上海");
+        addressMap.put("0002","广州");
+        Employee employee = new Employee();
+        employee.setName("ABC");
+        employee.setAddressMap(addressMap);
+        session.save(employee);
         if (transaction.getStatus().equals(TransactionStatus.ACTIVE)){
             transaction.commit();
         }
